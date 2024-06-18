5aa03734680ceaa883fd04709f3fe3463680a1d8
MDorizon <mdorizon@proton.me>
2024-06-18 16:39:24 +0200

    :sparkles: third step

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import FirstStepForm from './views/FirstStepForm.vue'
 import SecondStepChoice from './views/SecondStepEventChoice.vue'
 
 import { createRouter, createMemoryHistory } from 'vue-router'
+import ThirdStepChoice from './views/ThirdStepChoice.vue'
 
 const routes = [
     { path: '/', component: Accueil },
     { path: '/inscription/step1', component: FirstStepForm },
     { path: '/inscription/step2', component: SecondStepChoice },
+    { path: '/inscription/step3/:id', name: 'step3', component: ThirdStepChoice, props: true },
 ]
 
 const router = createRouter({
--- src/views/ThirdStepChoice.vue
+++ src/views/ThirdStepChoice.vue
@@ -1,13 +1,15 @@
 <script setup>
     import ProgressionStep from '../components/ProgressionStep.vue';
-
     import { ref, computed, onMounted } from 'vue'
+    import { defineProps } from 'vue';
 
-    const activityList = ref([])
+    const props = defineProps(['id']);
+
+    const activity = ref([])
     const fetchAllActivities = computed(async () => {
         const response = await fetch('/data_activities.json')
         const data = await response.json()
-        activityList.value = data
+        activity.value = data[props.id]
     })
 
     onMounted(() => {
@@ -16,12 +18,14 @@
 </script>
 
 <template>
-    <div class="card" v-for="(activity, i) in activityList">
-        <h2>{{ activity.name }}</h2>
+    <div class="card" v-for="session in activity.sessions">
+        <h2>{{ session.date }} || {{ session.start_at }} - {{ session.end_at }}</h2>
+        <p>{{ session.option }}</p>
+        <p>places prises : {{ session.registered }} / places restantes : {{ session.available_places }}</p>
         <RouterLink class="btn btn-primary" :to="{ name: 'step3', params: { id: i }}">Choisir</RouterLink>
     </div>
 
     <div class="d-flex justify-content-center">
-        <ProgressionStep currentStep=2 />
+        <ProgressionStep currentStep=3 />
     </div>
 </template>
