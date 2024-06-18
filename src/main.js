491595bfc4224469254fd065ecd9505b61c8f05b
MDorizon <mdorizon@proton.me>
2024-06-18 15:09:56 +0200

    :sparkles: added vue router

--- src/main.js
+++ src/main.js
@@ -2,4 +2,23 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Accueil from './views/Home.vue'
+import FirstStepForm from './views/FirstStepForm.vue'
+import SecondStepChoice from './views/SecondStepEventChoice.vue'
+
+import { createRouter, createMemoryHistory } from 'vue-router'
+
+const routes = [
+    { path: '/', component: Accueil },
+    { path: '/inscription/step1', component: FirstStepForm },
+    { path: '/inscription/step2', component: SecondStepChoice },
+]
+
+const router = createRouter({
+    history:createMemoryHistory(),
+    routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount("#app")
