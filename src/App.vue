417e55bce8189a843ae9d5e99e408162884e17fa
MDorizon <mdorizon@proton.me>
2024-06-18 13:44:00 +0200

    :construction: first step form

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from 'vue'
+  import FirstStepForm from './views/FirstStepForm.vue';
+
+  const PersonalData = ref([])
+
+  const FirstFormDatas = (persoData) => { 
+    PersonalData.value.push(persoData)
+    console.log(PersonalData.value)
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="mt-5 d-flex align-items-center flex-column">
+    <FirstStepForm @submitFirstForm="FirstFormDatas" />
+  </section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  @import "../node_modules/bootstrap/scss/bootstrap.scss";
+  a {
+    all: unset;
+    text-decoration: none;
+  }
 </style>
---
+++ src/views/FirstStepForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const emit = defineEmits()
+
+    const persoInfos = ref({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        zipCode: ''
+    })
+
+    const handleSubmit = () => {
+        emit('submitFirstForm', JSON.parse(JSON.stringify(persoInfos.value)))
+    }
+</script>
+
+<template>
+        <h2>Informations personnelles</h2>
+        <form class="col-8 mt-5 mb-5" @submit.prevent="handleSubmit">
+            <div class="mb-3 gap-3">
+                <input type="text" class="form-control mb-3" name="first-name" placeholder="prénom" v-model="persoInfos.firstName">
+                <input type="text" class="form-control mb-3" name="last-name" placeholder="nom" v-model="persoInfos.lastName">
+                <input type="text" class="form-control mb-3" name="email" placeholder="email" v-model="persoInfos.email">
+                <input type="text" class="form-control mb-3" name="phone" placeholder="téléphone" v-model="persoInfos.phone">
+                <input type="text" class="form-control" name="zip-code" placeholder="code postal" v-model="persoInfos.zipCode">
+            </div>
+            <div class="mb-3 gap-3">
+                <button type="submit" class="btn btn-primary w-100">Submit</button>
+            </div>
+        </form>
+</template>
